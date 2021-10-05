454f491c974581a69a293218302146fa8e152a8c
Yoga Pranata <45356875+zYxDevs@users.noreply.github.com>
2021-10-05 15:41:53 +0700

    Create bot-data.py

---
+++ IDNCoderX/modules/bot-data.py
@@ -0,0 +1,138 @@
+# Copyright (c) 2021 Itz-fork
+# Edited IDN-C-X
+
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+
+# Inline buttons
+class Buttons:
+    START_BUTTON=InlineKeyboardMarkup(
+        [
+            [
+                InlineKeyboardButton("Help 📜", callback_data="helpcallback"),
+                InlineKeyboardButton("About ⁉️", callback_data="aboutcallback")
+            ]
+        ]
+    )
+    
+    CHOOSE_E_BTN=InlineKeyboardMarkup(
+        [
+            [
+                InlineKeyboardButton("Extract 📂", callback_data="extract_file|no_pass"),
+                InlineKeyboardButton("(Password) Extract 📂", callback_data="extract_file|with_pass")
+            ],
+            [
+                InlineKeyboardButton("Cancel ❌", callback_data="cancel_dis")
+            ]
+        ]
+    )
+
+    CLN_BTNS=InlineKeyboardMarkup(
+        [
+            [
+                InlineKeyboardButton("Clean My Files 😇", callback_data="cancel_dis")
+            ],
+            [
+                InlineKeyboardButton("TF! Nooo 😳", callback_data="nobully")
+            ]
+        ]
+    )
+    
+    ME_GOIN_HOME=InlineKeyboardMarkup(
+        [
+            [
+                InlineKeyboardButton("Back 🏡", callback_data="megoinhome")
+            ]
+        ]
+    )
+
+
+class Messages:
+    START_TEXT = """
+Hi **{}**, I'm **IDNCoderX Unzipper Bot** 😇!
+
+`I can extract archives like zip, rar, tar etc.`
+
+**Made with ❤️ by @IDNCoder**
+    """
+
+    HELP_TXT = """
+**How To Extract? 🤔**
+
+`1. Send the file that you want to extract.`
+`2. Click on extract button.`
+
+
+**Note:**
+    **1.** `If your archive is password protected select` **(Password) Extract 📂** `mode. Bot isn't a GOD to know your file's password so If this happens just send that password!`
+    
+    **2.** `Please don't send corrupted files! If you sent a one by a mistake just send` **/clean** `command!`
+    
+    **3.** `If your archive have 95 or more files in it then bot can't show all of extracted files to select from. So in that case if you can't see your file in the buttons just click on` "Upload All ♻️" `button. It'll send all extracted files to you!`
+    """
+
+    ABOUT_TXT = """
+**About Nexa Unzipper Bot,**
+
+✘ **Language:** [Python](https://www.python.org/)
+✘ **Framework:** [Pyrogram](https://docs.pyrogram.org/)
+✘ **Source Code:** [IDN-C-X/IDN-Unzip-Bot](https://github.com/IDN-C-X/IDN-UnzipBot)
+✘ **Developer:** [zYxDevs](https://github.com/zYxDevs)
+
+
+**Made with ❤️ by @IDNCoder**
+    """
+
+    LOG_TXT = """
+**Extract Log 📝!**
+
+**User ID:** `{}`
+**File Name:** `{}`
+**File Size:** `{}`
+    """
+
+    AFTER_OK_DL_TXT = """
+**Successfully Downloaded**
+
+**Download time:** `{}`
+**Status:** `Trying to extract the archive`
+    """
+
+    EXT_OK_TXT = """
+**Extraction Successfull!**
+
+**Extraction time:** `{}`
+**Status:** `Trying to upload`
+    """
+
+    EXT_FAILED_TXT = """
+**Extraction Failed 😕!**
+
+**What to do?**
+
+ - `Please make sure archive isn't corrupted`
+ - `Please make sure that you selected the right mode!`
+ - `May be Your archive format isn't supported 😔`
+
+**Please report this at @Nexa_bots if you think this is a serious error**
+    """
+
+    ERROR_TXT = """
+**Error Happend 😕!**
+
+**ERROR:** {}
+
+
+**Please report this at @Nexa_bots if you think this is a serious error**
+    """
+
+    CANCELLED_TXT = """
+**{} ✅!**
+
+`Now all of your files have been deleted from my server 😏!`
+    """
+
+    CLEAN_TXT = """
+**Are sure want to delete your files from my server 🤔?**
+
+**Note:** `This action cannot be undone!`
+    """
